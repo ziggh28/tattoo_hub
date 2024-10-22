246d037d447ea6de465311ee0e506e003fc4beaa
ethanhuang33 <102780215+ethanhuang33@users.noreply.github.com>
2024-10-22 10:41:58 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+// scripts.js
+function sendMail(){
+    event.preventDefault();
+    let parms = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        pnumber : document.getElementById("pnumber").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value,
+    }                              
+    emailjs.send("service_ndsvvl7","template_bv7ciun",parms).then(alert("Booking Sent!"))
+}
 document.addEventListener('DOMContentLoaded', () => {
     const styleSearch = document.getElementById('styleSearch');
     const styleList = document.getElementById('styleList');
@@ -16,4 +28,5 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
 
